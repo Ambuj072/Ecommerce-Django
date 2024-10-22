a61a6931049293150d4ee254935fe03820354089
Ambuj Dubey <ambujdubey072@gmail.com>
2024-10-22 23:47:54 +0530

    product add

--- ecommerce/ecommerceapp/admin.py
+++ ecommerce/ecommerceapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from ecommerceapp.models import Contact
+from ecommerceapp.models import Contact,Product
 
 # Register your models here.
 admin.site.register(Contact)
+admin.site.register(Product)
 
